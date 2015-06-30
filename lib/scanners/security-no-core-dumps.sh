b0a86718d3566bccc6dabb9fe5b3ed639cdd49c1
Lars Windolf <lars.windolf@gmx.de>
2015-06-30 15:20:34 +0200

    Policy for core dumps disabled.

---
+++ lib/scanners/security-no-core-dumps.sh
@@ -0,0 +1,8 @@
+# group: Security
+# name: No Core Dumps
+# description: Checks if core dumps are disabled in /etc/security/limits.conf and /etc/security/limits.d/*
+
+output=$(grep "core[:space:][:space:]*[^0]" /etc/security/limits.conf /etc/security/limits.d/* 2>/dev/null)
+if [ "$output" != "" ]; then
+	result_warning "Core dumps are not disabled!"
+fi
