40b98ac1ffde10d4d03beaa0e59a157e22f7d21c
Lars Windolf <lars.windolf@gmx.de>
2015-07-02 14:00:20 +0200

    Added policy for SysRq disabled.

---
+++ lib/scanners/security-sysrq-disabled.sh
@@ -0,0 +1,8 @@
+# group: Security
+# name: No SysRq
+# description: /proc/sys/kernel/sysrq is to be 0
+
+if [[ $(sysctl -n /proc/sys/kernel/sysrq 2>/dev/null) != "0" ]]; then
+	result_failed "sysctl /proc/sys/kernel/sysrq != 0"
+fi
+
