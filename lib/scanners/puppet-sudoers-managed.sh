5531a03118db03993b82c8b95b071edfb68f0b1e
Lars Windolf <lars.windolf@idealo.de>
2015-07-06 15:47:40 +0200

    Policy for Puppet managed /etc/sudoers*

---
+++ lib/scanners/puppet-sudoers-managed.sh
@@ -0,0 +1,23 @@
+# group: Puppet
+# name: /etc/sudoers* managed
+# description: Checks for Puppet 2.7 wether all sudoers definitions are managed
+
+sudoers_files=$(ls /etc/sudoers.d/* 2>/dev/null)
+unmanaged=$(
+for f in $sudoers_files; do
+	if ! grep -q "resource: File\[$f\]" /var/lib/puppet/state/last_run_report.yaml 2>/dev/null; then
+		echo $f
+	fi
+done
+)
+
+if [ "$unmanaged" != "" ]; then
+	result_failed "Unmanaged files found:" $unmanaged
+else
+	result_ok
+fi
+
+tmp=$(egrep -v "^#|^Defaults|^[[:space:]]*$|^root[[:space:]]|^%sudo[[:space:]]" /etc/sudoers)
+if [ "$tmp" != "" ]; then
+	result_failed "Unexpected entries in /etc/sudoers (all entries should be Puppet managed and in separate files in /etc/sudoers.d/): $tmp"
+fi
