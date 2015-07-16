d48eba8d058233e01556b940a53eda7dc8ec074b
Lars Windolf <lars.windolf@gmx.de>
2015-07-16 18:05:49 +0200

    Update security-aslr-enabled.sh

--- lib/scanners/security-aslr-enabled.sh
+++ lib/scanners/security-aslr-enabled.sh
@@ -1,6 +1,7 @@
 # group: Security
 # name: ASLR enabled
 # description: Address Space Layout Randomization is to be enabled.
+# tags: CCE-27007-4
 
 
 if [[ $(sysctl -n kernel.randomize_va_space 2>/dev/null) != "2" ]]; then
