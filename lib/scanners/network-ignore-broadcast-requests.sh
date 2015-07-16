8589a21c7d9b15cedbc0762d92f2b067c9485a44
Lars Windolf <lars.windolf@gmx.de>
2015-07-16 18:25:01 +0200

    Update network-ignore-broadcast-requests.sh

--- lib/scanners/network-ignore-broadcast-requests.sh
+++ lib/scanners/network-ignore-broadcast-requests.sh
@@ -1,6 +1,7 @@
 # group: Network
 # name: Ignore Broadcasts
 # description: Ensures that ICMP broadcast requests are ignored
+# tags: CCE-26883-9
 
 if [[ $(sysctl -n net.ipv4.icmp_echo_ignore_broadcasts 2>/dev/null) == 1 ]]; then
 	result_failed "net.ipv4.icmp_echo_ignore_broadcasts is not 1"
