5e925e61bfe4088a5be9c55edb7614dfd716082b
Lars Windolf <lars.windolf@gmx.de>
2015-07-16 19:41:26 +0200

    Fix condition

--- lib/scanners/security-ntpd-active.sh
+++ lib/scanners/security-ntpd-active.sh
@@ -3,6 +3,6 @@
 # description: There must be a running ntpd
 # tags: CCE-27093-4
 
-if pgrep -f ntpd >/dev/null; then
+if ! pgrep -f ntpd >/dev/null; then
 	result_failed "No running 'ntpd' process found!"
 fi
