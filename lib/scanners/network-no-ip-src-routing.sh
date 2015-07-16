591f944f9d80f5ba906c60d920e00842af66f012
Lars Windolf <lars.windolf@idealo.de>
2015-07-16 18:24:00 +0200

    Add policy for Intel NX enabled. Add CCE tag for network-no-ip-src-routing.sh

--- lib/scanners/network-no-ip-src-routing.sh
+++ lib/scanners/network-no-ip-src-routing.sh
@@ -1,6 +1,7 @@
 # group: Network
 # name: No IP Source Routing
 # description: Ensures that IP Source Routing is disabled
+# tags: CCE-27037-1
 
 if [[ $(sysctl -n net.ipv4.conf.all.accept_source_route 2>/dev/null) == 0 ]]; then
 	result_failed "net.ipv4.conf.all.accept_source_route is not 0"
---
+++ lib/scanners/security-nx-enabled.sh
@@ -0,0 +1,10 @@
+# group: Security
+# name: Execute Disable Support
+# description: On Intel CPUs execute disable protection should be active
+# tags: CCE-27001-4
+
+
+if [[ $(dmesg | grep '[NX|DX]*protection: active') != "" ]]; then
+	result_failed "Intel Execute Disable support not active!"
+fi
+
