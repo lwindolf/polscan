82b58d111a89cc19a5b941e4ce77c1c8fdfc81a8
Lars Windolf <lars.windolf@gmx.de>
2015-07-16 18:31:12 +0200

    Update ssh-legacy-disabled.sh

--- lib/scanners/ssh-legacy-disabled.sh
+++ lib/scanners/ssh-legacy-disabled.sh
@@ -1,6 +1,7 @@
 # group: SSH
 # name: Legacy Options
 # description: Checks /etc/ssh/sshd_config for disabled legacy features: IgnoreRhosts yes, HostbasedAuthentication no, RhostsRSAAuthentication no, Protocol 2 (no 1)
+# tags: CCE-27124-7 CCE-27091-8 CCE-26887-0 
 
 if ! grep -q "IgnoreRhosts yes" /etc/ssh/sshd_config; then
 	result_failed "IgnoreRhosts is not set to yes"
@@ -8,6 +9,10 @@ fi
 
 if ! grep -q "HostbasedAuthentication no" /etc/ssh/sshd_config; then
 	result_failed "HostbasedAuthentication is not set to no"
+fi
+
+if ! grep -q "PermitEmptyPasswords no" /etc/ssh/sshd_config; then
+	result_failed "PermitEmptyPasswords is not set to no"
 fi
 
 if ! grep -q "RhostsRSAAuthentication no" /etc/ssh/sshd_config; then
