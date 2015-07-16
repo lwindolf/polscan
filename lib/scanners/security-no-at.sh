63d39daf6041cc9779f36b99d6663639cf12762a
Lars Windolf <lars.windolf@gmx.de>
2015-07-16 19:36:43 +0200

    Fixes package match

--- lib/scanners/security-no-at.sh
+++ lib/scanners/security-no-at.sh
@@ -3,7 +3,7 @@
 # description: The atd daemon must not be installed
 # tags: CCE-27249-2
 
-dpkg -l at >/dev/null 2>&1
+dpkg -l at | grep '^ii' >/dev/null 2>&1
 if [ $? -eq 0 ]; then
 	result_failed "Package at is installed"
 fi
--- lib/scanners/security-no-autofs.sh
+++ lib/scanners/security-no-autofs.sh
@@ -3,7 +3,7 @@
 # description: The automounter must not be installed
 # tags: CCE-26976-1
 
-dpkg -l 'autofs*' >/dev/null 2>&1
+dpkg -l 'autofs*' | grep '^ii' >/dev/null 2>&1
 if [ $? -eq 0 ]; then
 	result_failed "Package autofs is installed"
 fi
