549d0fc8395c7387a80461af90e3eaefd0555be9
Lars Windolf <lars.windolf@gmx.de>
2015-07-17 16:19:55 +0200

    Checking selinux=0 not in grub.cfg

--- lib/scanners/security-selinux-enabled.sh
+++ lib/scanners/security-selinux-enabled.sh
@@ -1,7 +1,13 @@
 # group: Security
 # name: SELinux enabled
-# description: Uses 'sestatus' to check if SELinux is enabled.
+# description: Uses 'sestatus' to check if SELinux is enabled. Checks grub.cfg for not having selinux=0.
+# tags: CCE-26956-3
 
 if ! sestatus 2>/dev/null | grep -q enabled; then
 	result_failed "sestatus does not report 'enabled'"
 fi
+
+if grep -q "^[[:space:]]*[^#]selinux=0" /boot/grub/grub.cfg; then
+	result_failed "SELinux disabled in grub.cfg"
+fi
+
