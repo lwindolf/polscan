6f08b3e40cafe34ec0d5b987460c96351e77ca29
Lars Windolf <lars.windolf@gmx.de>
2015-07-17 17:03:36 +0200

    Add securetty policy.

---
+++ lib/scanners/security-securetty.sh
@@ -0,0 +1,12 @@
+# group: Security
+# name: securetty
+# description: No root login via virtual consoles and serial ports
+# tags: CCE-26855-7 CCE-27047-0
+
+insecure=$(egrep '^(ttyS[0-9]|vc/[0-9])[[:space:]]*$' /etc/securetty)
+if [ "$insecure" != "" ]; then
+	result_failed "/etc/securetty allows root login via $insecure"
+else
+	result_ok
+fi
+
