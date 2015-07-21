ce30d66fd9c7fdaa98a96904b63bedc4a3fff9c8
Lars Windolf <lars.windolf@gmx.de>
2015-07-21 16:58:54 +0200

    Support for archive security repo too

--- lib/scanners/security-repo-enabled.sh
+++ lib/scanners/security-repo-enabled.sh
@@ -2,7 +2,7 @@
 # name: Security Repo
 # description: Ensure the security.debian.org repo is enabled
 
-if grep -q '^deb[[:space:]]*http://security.debian.org.*main' /etc/apt/sources.list /etc/apt/sources.list.d/* 2>/dev/null; then
+if egrep -q '^deb[[:space:]]*(http://security.debian.org|http://archive.debian.org.*security).*main' /etc/apt/sources.list /etc/apt/sources.list.d/* 2>/dev/null; then
 	result_ok
 else
 	result_failed "Debian security updates repo not found in /etc/apt/source.list*!"
