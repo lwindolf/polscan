017e0eae3871de7486c69a76570c117ed9ee0003
Lars Windolf <lars.windolf@gmx.de>
2015-09-03 19:07:57 +0200

    Update network-syn-cookies-on.sh

--- lib/scanners/network-syn-cookies-on.sh
+++ lib/scanners/network-syn-cookies-on.sh
@@ -1,7 +1,7 @@
 # group: Network
 # name: SYN Cookie Protection
 # description: Ensures that SYN cookies are enabled.
-# tags: CCE-27053-8
+# source: http://people.redhat.com/swells/scap-security-guide/RHEL/7/output/table-rhel7-cces.html
 # solution-cmd: echo 'net.ipv4.tcp_syncookies=1' >/etc/sysctl.d/50-net.ipv4.tcp_syncookies.conf
 
 if [[ $(/sbin/sysctl -n net.ipv4.tcp_syncookies 2>/dev/null) == 0 ]]; then
