c21e80f9cdadde37ea17ebb92ff07d8a66400e40
Lars Windolf <lars.windolf@gmx.de>
2015-09-03 19:09:44 +0200

    Update network-ignore-broadcast-requests.sh

--- lib/scanners/network-ignore-broadcast-requests.sh
+++ lib/scanners/network-ignore-broadcast-requests.sh
@@ -1,7 +1,7 @@
 # group: Network
 # name: Ignore Broadcasts
 # description: Ensures that ICMP broadcast requests are ignored
-# tags: CCE-26883-9
+# source: http://people.redhat.com/swells/scap-security-guide/RHEL/7/output/table-rhel7-cces.html
 # solution-cmd: echo 'net.ipv4.icmp_echo_ignore_broadcasts=1' >/etc/sysctl.d/50-icmp_echo_ignore_broadcasts.conf && sysctl -p
 
 if [[ $(/sbin/sysctl -n net.ipv4.icmp_echo_ignore_broadcasts 2>/dev/null) == 0 ]]; then
