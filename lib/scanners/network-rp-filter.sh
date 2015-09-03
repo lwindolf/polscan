eb72e77c958cf98f869b1743afe636e589194146
Lars Windolf <lars.windolf@gmx.de>
2015-09-03 19:14:19 +0200

    Update network-rp-filter.sh

--- lib/scanners/network-rp-filter.sh
+++ lib/scanners/network-rp-filter.sh
@@ -1,7 +1,10 @@
 # group: Network
 # name: IP Spoofing
 # description: Ensures that IP spoofing protection is enabled
-# tags: CCE-26979-5
+# source: http://people.redhat.com/swells/scap-security-guide/RHEL/7/output/table-rhel7-cces.html
+# source: http://www.linuxtopia.org/online_books/linux_system_administration/securing_and_optimizing_linux/chap5sec60.html
+# source: http://www.linuxsecurity.com/resource_files/documentation/tcpip-security.html
+# source: http://www.cyberciti.biz/tips/linux-iptables-8-how-to-avoid-spoofing-and-bad-addresses-attack.html
 # solution-cmd: echo 'net.ipv4.conf.all.rp_filter=1' >/etc/sysctl.d/50-net.ipv4.conf.all.rp_filter.conf
 
 if [[ $(/sbin/sysctl -n net.ipv4.conf.all.rp_filter 2>/dev/null) == 1 ]]; then
