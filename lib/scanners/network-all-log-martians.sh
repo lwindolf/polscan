a2e451ba81807dbdd33286bdb3592f4835bf5e76
Lars Windolf <lars.windolf@gmx.de>
2015-09-03 19:09:18 +0200

    Update network-all-log-martians.sh

--- lib/scanners/network-all-log-martians.sh
+++ lib/scanners/network-all-log-martians.sh
@@ -1,6 +1,7 @@
 # group: Network
 # name: All Log Martians
 # description: Ensures logging of all suspicious packages
+# source: http://people.redhat.com/swells/scap-security-guide/RHEL/7/output/table-rhel7-cces.html
 # solution-cmd: echo 'net.ipv4.conf.all.log_martians=1' >/etc/sysctl.d/50-net.ipv4.conf.all.log_martians.conf && sysctl -p
 
 if [[ $(/sbin/sysctl -n net.ipv4.conf.all.log_martians 2>/dev/null) == 1 ]]; then
