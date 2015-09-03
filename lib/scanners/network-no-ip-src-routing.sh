a3a59a4ebffb99b5238e237c5bbce54cbdac76d0
Lars Windolf <lars.windolf@gmx.de>
2015-09-03 19:08:46 +0200

    Update network-no-ip-src-routing.sh

--- lib/scanners/network-no-ip-src-routing.sh
+++ lib/scanners/network-no-ip-src-routing.sh
@@ -1,8 +1,8 @@
 # group: Network
 # name: No IP Source Routing
 # description: Ensures that IP Source Routing is disabled
-# tags: CCE-27037-1
-# solution-cmd: echo 'net.ipv4.conf.all.accept_source_route' >/etc/sysctl.d/50-net.ipv4.conf.all.accept_source_route.conf && sysctl -p
+# source: http://people.redhat.com/swells/scap-security-guide/RHEL/7/output/table-rhel7-cces.html
+# solution-cmd: echo 'net.ipv4.conf.all.accept_source_route = 0' >/etc/sysctl.d/50-net.ipv4.conf.all.accept_source_route.conf && sysctl -p
 
 if [[ $(/sbin/sysctl -n net.ipv4.conf.all.accept_source_route 2>/dev/null) != 0 ]]; then
 	result_failed "net.ipv4.conf.all.accept_source_route is not 0"
