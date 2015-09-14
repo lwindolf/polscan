e894fca377793a0c6e0f775aadd29230d139d363
Lars Windolf <lars.windolf@gmx.de>
2015-09-14 18:02:08 +0200

    Fix sysctl key error. Suppress permission denied on net.ipv?.route.flush.

--- lib/scanners/security-ipv4-forwarding.sh
+++ lib/scanners/security-ipv4-forwarding.sh
@@ -3,6 +3,6 @@
 # description: IPv4 forwarding is to be disabled
 # tags: CCE-26866-4
 
-if /sbin/sysctl net.ipv4 | grep -q "\.forwarding = 1" 2>/dev/null; then
+if /sbin/sysctl -a 2>/dev/null | grep -q "^net\.ipv4\.*\.forwarding = 1" 2>/dev/null; then
 	result_failed "IPv4 forwarding is enabled"
 fi
--- lib/scanners/security-ipv6-forwarding.sh
+++ lib/scanners/security-ipv6-forwarding.sh
@@ -2,6 +2,6 @@
 # name: No IPv6 forwarding
 # description: IPv6 forwarding is to be disabled
 
-if /sbin/sysctl net.ipv6 | grep -q "\.forwarding = 1" 2>/dev/null; then
+if /sbin/sysctl -a 2>/dev/null | grep -q "^net\.ipv6\..*\.forwarding = 1" 2>/dev/null; then
 	result_failed "IPv6 forwarding is enabled"
 fi
