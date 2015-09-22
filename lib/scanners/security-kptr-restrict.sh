0534af5e604b85cdd9238795357827ad88bbf06c
Lars Windolf <lars.windolf@gmx.de>
2015-09-22 16:50:51 +0200

    Silence complain when kptr sysctl switch doesn't exist.

--- lib/scanners/security-kptr-restrict.sh
+++ lib/scanners/security-kptr-restrict.sh
@@ -4,7 +4,7 @@
 # solution-cmd: echo 'kernel.kptr_restrict = 1' >/etc/sysctl.d/50-kptr-restrict.conf && sysctl -p
 # source: https://wiki.archlinux.org/index.php/Security
 
-if [ $(/sbin/sysctl -n kernel.kptr_restrict) == "1" ]; then
+if [ $(/sbin/sysctl -n kernel.kptr_restrict 2>/dev/null) == "1" ]; then
 	result_ok
 else
 	result_failed "Kernel symbols in /proc/kallsyms are not restricted to root only!"
