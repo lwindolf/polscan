26efc923ea8222e73d6b659e1450278621c01959
Lars Windolf <lars.windolf@gmx.de>
2015-10-07 21:56:44 +0200

    Policy for USB drives.

---
+++ lib/scanners/system-usb-drives.sh
@@ -0,0 +1,10 @@
+# group: System
+# name: USB Drives
+# description: On server systems USB disk devices are not normal. So you might want to know about all of them.
+
+disks=$(ls /dev/disk/by-id/usb* 2>/dev/null)
+if [ "$disks" == "" ]; then
+	result_ok
+else
+	result_warning "USB disk devices found: $disks"
+fi
