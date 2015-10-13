7fa90619af0fde6a38abdd77345e82be766dc3df
Lars Windolf <lars.windolf@gmx.de>
2015-10-13 16:51:12 +0200

    Add policy for OOM kernel messages

---
+++ lib/scanners/system-oom-logged.sh
@@ -0,0 +1,10 @@
+# group: System
+# name: OOM logged
+# description: There shall be no 'Out of memory: kill process' in /var/log/dmesg as they usually indicate insufficient memory or memory leaks.
+
+logged=$(/bin/dmesg | grep 'Out of memory: kill process' | tail -10)
+if [ "$logged" == "" ]; then
+	result_ok
+else
+	result_failed "dmesg has 'Out of memory: kill process' entries: $logged"
+fi
