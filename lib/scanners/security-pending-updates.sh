2686dac2a24934d0d7c7c72b40d5363d9aa9384f
Lars Windolf <lars.windolf@gmx.de>
2015-10-16 14:06:34 +0200

    Fix rogue echo

--- lib/scanners/security-pending-updates.sh
+++ lib/scanners/security-pending-updates.sh
@@ -4,7 +4,7 @@
 
 security_repos=$(grep -h '^deb.*security' /etc/apt/sources.list /etc/apt/sources.list.d/* 2>/dev/null)
 TMPFILE=$(mktemp) && {
-	echo "$security_repos" >$TMPFILE
+	/bin/echo "$security_repos" >$TMPFILE
 	pkgs=$(apt-get -s dist-upgrade -o Dir::Etc::SourceList=$TMPFILE 2>/dev/null | grep ^Inst | awk '{print $2}')
 	if [ "$pkgs" != "" ]; then
 		result_warning "Security upgrades pending:" $pkgs
