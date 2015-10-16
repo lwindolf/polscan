18acadcd120f9036ddb6337fda34dbffbc4ca14f
Lars Windolf <lars.windolf@gmx.de>
2015-10-16 19:01:48 +0200

    Hint on scanners working with Puppet 3 too.

--- lib/scanners/puppet-users-managed.sh
+++ lib/scanners/puppet-users-managed.sh
@@ -1,6 +1,6 @@
 # group: Puppet
 # name: Users managed
-# description: Checks for Puppet 2.7 wether all UID > 1000 are managed
+# description: Checks for Puppet 2/3 wether all UID > 1000 are managed
 
 if [ -d /var/lib/puppet/state ]; then
 	if ! grep -q "^  status: failed" /var/lib/puppet/state/last_run_report.yaml 2>/dev/null; then 
