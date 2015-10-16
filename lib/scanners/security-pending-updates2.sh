0c05d25798168368cb24fae604d7ac5ecfba0a9a
Lars Windolf <lars.windolf@gmx.de>
2015-10-16 13:48:50 +0200

    Fix apt-show-versions variant of 'Pending Updates' policy to accept all stable upgrades

--- lib/scanners/security-pending-updates2.sh
+++ lib/scanners/security-pending-updates2.sh
@@ -1,8 +1,8 @@
 # group: Security
 # name: Pending Updates
-# description: There should be no pending security updates. This check uses apt-show-versions which needs to be installed. 
+# description: There should be no pending updates. This check uses apt-show-versions which needs to be installed.
 
-pkgs=$(apt-show-versions 2>/dev/null | grep 'security upgradable' | cut -d: -f 1)
+pkgs=$(apt-show-versions 2>/dev/null | grep ' upgradable' | cut -d: -f 1)
 if [ "$pkgs" != "" ]; then
 	result_warning "Security upgrades pending:" $pkgs
 else
