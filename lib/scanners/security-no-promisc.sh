d6e697ce050a8d9abe23d0318c175657c3bae378
Lars Windolf <lars.windolf@gmx.de>
2015-10-22 01:01:26 +0200

    Added policy for network interfaces in promiscuous mode.

---
+++ lib/scanners/security-no-promisc.sh
@@ -0,0 +1,11 @@
+# group: Security
+# name: No promiscuous interface
+# description: Ensures there are no interfaces in promiscuous mode indicating package capturing
+# solution-cmd: /sbin/ip a | grep PROMISC | awk -F: '{print $2}' | xargs -n 1 -I "{}" ifconfig "{}" -promisc
+
+interfaces=$(/sbin/ip a | grep PROMISC | awk -F: '{print $2}')
+if [ "$interfaces" != "" ]; then
+	result_warning "Interfaces in promiscuous mode: $interfaces"
+else
+	result_ok
+fi
