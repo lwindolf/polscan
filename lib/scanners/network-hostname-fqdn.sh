0a96d0420dfb6c03a5e7ef4a5c20294750ef9b7d
Lars Windolf <lars.windolf@gmx.de>
2015-10-22 14:37:57 +0200

    Fix FQDN hostname check.

--- lib/scanners/network-hostname-fqdn.sh
+++ lib/scanners/network-hostname-fqdn.sh
@@ -2,7 +2,7 @@
 # name: FQDN
 # description: Ensure that the server has an FQDN configured.
 
-if [[ $(/bin/hostname -f) ~ \. ]]; then
+if [[ $(/bin/hostname -f) =~ \. ]]; then
 	result_ok
 else
 	result_failed "'/bin/hostname -f' doesn't return an FQDN!"
