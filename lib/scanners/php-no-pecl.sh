c4c5f299944c2bbd8011ea057c3bb3917d2b4f3f
Lars Windolf <lars.windolf@gmx.de>
2015-10-28 12:08:00 +0100

    Policy for not using PECL.

---
+++ lib/scanners/php-no-pecl.sh
@@ -0,0 +1,10 @@
+# group: PHP
+# name: No PECL
+# description: Checks there are no PECL installed modules at all. This makes sense when using disto packages or self-build packages exclusively.
+
+packages=$(pecl list 2>/dev/null |grep -A1000 ^PACKAGE | grep -v "PACKAGE")
+if [ "$packages" != "" ]; then
+	result_failed "Unexpected PECL packages: $packages"
+else
+	result_ok
+fi
