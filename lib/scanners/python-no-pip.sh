57215bd5d183d79f6e7fd49ae00c72b453981dd3
Lars Windolf <lars.windolf@gmx.de>
2015-11-02 19:41:12 +0100

    Policy for no Python pip packages.

---
+++ lib/scanners/python-no-pip.sh
@@ -0,0 +1,10 @@
+# group: Python
+# name: No Pip
+# description: Checks there are no Pip installed packages at all. This makes sense when using distro packages or self-build packages exclusively.
+
+packages=$(pip freeze 2>/dev/null)
+if [ "$packages" != "" ]; then
+	result_failed "Unexpected Python packages: $packages"
+else
+	result_ok
+fi
