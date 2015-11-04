6db07bed72a1890c1e17ce564ca5e19b44413c19
Lars Windolf <lars.windolf@gmx.de>
2015-11-04 13:57:10 +0100

    Policy for internet reachable.

---
+++ lib/scanners/network-internet-reachable.sh
@@ -0,0 +1,9 @@
+# group: Network
+# name: Internet Reachable
+# description: Ensures that outbound internet traffic is possible. Useful when you use external APT repos other other internet services.
+
+if /bin/ping -W 1 -c 1 8.8.8.8 >/dev/null 2>&1; then
+	result_ok
+else
+	result_failed "Ping 8.8.8.8 failed"
+fi
