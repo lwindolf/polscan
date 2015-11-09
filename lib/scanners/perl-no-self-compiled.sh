7abff289acf5bb396d2dd3d15b36ff349735f280
Lars Windolf <lars.windolf@gmx.de>
2015-11-09 19:09:13 +0100

    Added policy for no self-compiled Perl modules.

---
+++ lib/scanners/perl-no-self-compiled.sh
@@ -0,0 +1,20 @@
+# group: Perl
+# name: No self-compiled
+# description: Checks there are no self-compiled Perl modules. Fuzzy check that takes existing directories as indication for those.
+
+bad_dirs=
+for s in $(
+	perl -e 'print qq(@INC)' 2>/dev/null |\
+	xargs -n 1 |\
+	egrep -v '^/usr/share/|^/usr/lib|^/etc|^\.$'
+); do
+	if [ -d "$s" ]; then
+		bad_dirs="${bad_dirs}$s "
+	fi
+done
+
+if [ "$bad_dirs" != "" ]; then
+	result_failed "Unexpected Perl module directories: $bad_dirs"
+else
+	result_ok
+fi
