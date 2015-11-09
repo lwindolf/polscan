333f9295ba1dcc60228caa86a627fddc6d7eab67
Lars Windolf <lars.windolf@gmx.de>
2015-11-09 16:45:53 +0100

    Policy to check for PHP module load errors

---
+++ lib/scanners/php-modules-load.sh
@@ -0,0 +1,13 @@
+# group: PHP
+# name: Modules Load
+# description: Checks if all modules do load. Fuzzy check that uses the "php" from path. This might not always be the one you want.
+
+php=$(which php)
+if [ -f "$php" ]; then
+	fails=$($php -m 2>&1 >/dev/null)
+	if [ "$fails" != "" ]; then
+		result_failed "$php module load failures: $fails"
+	else
+		result_ok
+	fi
+fi
