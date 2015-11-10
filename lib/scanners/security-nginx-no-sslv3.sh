c15e1c3bf994c535176211ed929fd4595d0f3d52
Lars Windolf <lars.windolf@gmx.de>
2015-11-10 12:03:38 +0100

    Policy for nginx SSLv2/3 protocol and prefer server cipher.

---
+++ lib/scanners/security-nginx-no-sslv3.sh
@@ -0,0 +1,12 @@
+# group: Security
+# name: nginx Prefer Server Ciphers
+# description: An nginx production webserver should prefer the server side ciphers.
+# source: https://raymii.org/s/tutorials/Strong_SSL_Security_On_nginx.html
+
+for dir in /etc/nginx /usr/local/nginx/conf; do
+	if [ -d $dir ]; then
+		if rgrep -q "ssl_protocols[[:space:]][[:space:]]*SSLv[23]" $dir/*-enabled $dir/conf.d; then
+			result_failed "ssl_protocols includes SSLv[2/3]"
+		fi
+	fi
+done
---
+++ lib/scanners/security-nginx-prefer-server-ciphers.sh
@@ -0,0 +1,12 @@
+# group: Security
+# name: nginx Prefer Server Ciphers
+# description: An nginx production webserver should prefer the server side ciphers.
+# source: https://raymii.org/s/tutorials/Strong_SSL_Security_On_nginx.html
+
+for dir in /etc/nginx /usr/local/nginx/conf; do
+	if [ -d $dir ]; then
+		if ! rgrep -q "ssl_prefer_server_ciphers[[:space:]][[:space:]]*on" $dir/*-enabled $dir/conf.d; then
+			result_failed "ssl_prefer_server_ciphers is not set to 'on'"
+		fi
+	fi
+done
