afbe4f4830d2ff71c8f33fe97c225ad9698483ce
Lars Windolf <lars.windolf@gmx.de>
2015-11-13 14:01:34 +0100

    Adapt 'Puppet not disabled' policy to work with Puppet 3

--- lib/scanners/puppet-not-disabled.sh
+++ lib/scanners/puppet-not-disabled.sh
@@ -1,9 +1,9 @@
 # group: Puppet
 # name: Not disabled
-# description: Checks for Puppet 2.7 puppetdlock file indicating Puppet runs are disabled
+# description: Checks for Puppet 2/3 lock file indicating Puppet runs are disabled
 # solution-cmd: /usr/bin/puppet agent --enable
 
-if [ ! -f /var/lib/puppet/state/puppetdlock ]; then
+if [ ! -f /var/lib/puppet/state/puppetdlock -a ! -f /var/lib/puppet/state/agent_disabled.lock ]; then
 	result_ok
 else
 	result_failed
