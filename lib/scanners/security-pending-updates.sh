a63e6f36ab84666a45ea51e848578afc677b8f5d
Lars Windolf <lars.windolf@gmx.de>
2015-11-17 13:46:30 +0100

    Fix parsing version variant. Document possible variants.

--- lib/scanners/security-pending-updates.sh
+++ lib/scanners/security-pending-updates.sh
@@ -6,9 +6,15 @@ security_repos=$(grep -h '^deb.*security' /etc/apt/sources.list /etc/apt/sources
 TMPFILE=$(mktemp) && {
 	/bin/echo "$security_repos" >$TMPFILE
 	pkgs=$(
+		# Typical apt-get -s dist-upgrade output:
+		#
+		# Inst liberror-perl (0.17-1 Debian:7.9/oldstable [all])
+		# Inst libfcgi0ldbl [2.4.0-8.1] (2.4.0-8.1+deb7u1 Debian:7.9/oldstable [amd64])
+		#
+		# Note the extra version in line 2
 		apt-get -s dist-upgrade -o Dir::Etc::SourceList=$TMPFILE 2>/dev/null |\
 		grep ^Inst |\
-		awk '{version=$4; gsub(/\(/, "", version); print $2 "=" version}'
+		awk '{if($3 ~ /\[/) { version=$4} else { version=$3 }; gsub(/\(/, "", version); print $2 "=" version}'
 	)
 	if [ "$pkgs" != "" ]; then
 		result_warning "Security upgrades pending:" $pkgs
