1f89ae29ea822a7e36f428b2a16cf9d9227d2228
Lars Windolf <lars.windolf@gmx.de>
2015-12-09 23:43:33 +0100

    Add policy for proper SYN flood mitigation reporting only on systems where dmesg indicates a SYN flood

---
+++ lib/scanners/network-tcp-max-syn-backlog.sh
@@ -0,0 +1,16 @@
+# group: System
+# name: SYN flooding
+# description: Ensures that SYN cookies are enabled and SYN backlog is configured on hosts with dmesg reporting SYN flood
+# source: https://www.frozentux.net/ipsysctl-tutorial/ipsysctl-tutorial.html#AEN398
+
+logged=$(/bin/dmesg | grep 'possible SYN flooding' | tail -10)
+if [ "$logged" != "" ]; then
+	if [[ $(/sbin/sysctl -n net.ipv4.tcp_syncookies 2>/dev/null) == 0 ]]; then
+		result_warning "SYN flood warning in dmesg and net.ipv4.tcp_syncookies is not enabled."
+	else
+		result_warning "$logged"
+	fi
+	if [[ $(/sbin/sysctl -n net.ipv4.tcp_max_syn_backlog 2>/dev/null) > 1024 ]]; then
+		result_critical "SYN flood warning in dmesg and net.ipv4.tcp_max_syn_backlog <= 1024."
+	fi
+fi
