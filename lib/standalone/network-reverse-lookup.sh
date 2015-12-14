3c5d5d99eb92143236ec183c5c6b2a72ccda3d24
Lars Windolf <lars.windolf@gmx.de>
2015-12-14 21:30:04 +0100

    Strip user from host names.

--- lib/standalone/network-reverse-lookup.sh
+++ lib/standalone/network-reverse-lookup.sh
@@ -3,6 +3,7 @@
 # description: Ensures that all hosts have reverse lookup properly configured
 
 for h in $HOST_LIST; do
+	h=${h/*@/}
 	ip=$(dig +short $h)
 	if [ "$ip" != "" ]; then
 		reverse=$(dig +short -x $ip)
