e2c813bc3926a851a41997e220fb48e8c60ed272
Lars Windolf <lars.windolf@gmx.de>
2016-01-05 18:27:38 +0100

    Move CVEs into own group for better mapping in 'Findings' map. Add an inventory type for unpatched CVE count. Extend output to CVE-package pairs.

--- lib/scanners/security-unpatched-cve.sh
+++ lib/scanners/security-unpatched-cve.sh
@@ -1,13 +1,21 @@
-# group: Security
+# group: Security-CVEs
 # name: Unpatched CVEs
-# description: There should be no urgent CVEs unpatched for which there are Debian updates available. Requires debsecan.
+# description: There should be no urgent CVEs unpatched for which there are Debian updates available. Requires debsecan. Also provides a count based inventory.
 # solution-cmd: apt-get install $(debsecan --only-fixed 2>&1 | grep "high urgency" | awk '{print $2}')
 
 if [ -f /usr/bin/debsecan ]; then 
-	cves=$(debsecan --only-fixed --suite $(lsb_release -cs) 2>/dev/null | grep 'high urgen' | cut -d " "  -f 1 | sort -u)
+	cves=$(/usr/bin/debsecan --only-fixed --suite $(lsb_release -cs) 2>/dev/null | awk '/high urgency/{print $1 ":" $2}' | sort -u)
 	if [ "$cves" != "" ]; then
-		result_critical "Unpatched CVE: $cves"
+		result_failed "Unpatched CVE: " $cves
 	else
 		result_ok
 	fi
+
+	count=$(echo $cves | grep -v '^ *$' | wc -w)
+	if [ $count -gt 10 ]; then
+		count=$(( $count / 10 * 10 ))
+	fi
+	result_inventory "Unpatched Count" $count
+else
+	result_warning "debscan is not installed"
 fi
