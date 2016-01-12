99f7ff887e0b8b82274b39fd8c0fa618668880d3
Lars Windolf <lars.windolf@gmx.de>
2016-01-12 16:21:24 +0100

    Add locale and timezone to system inventory.

--- lib/scanners/system-inventory.sh
+++ lib/scanners/system-inventory.sh
@@ -1,8 +1,10 @@
 # group: System
 # name: Inventory
-# description: Inventory only scanner determining kernel version with uname -r, CPU+RAM GB count, OS release, RAID vendor, Uptime
+# description: Inventory only scanner determining kernel version with uname -r, locale, timezone, CPU+RAM GB count, OS release, RAID vendor, Uptime
 
 result_inventory "Kernel Version" $(uname -r)
+result_inventory "Default Locale" $LANG
+result_inventory "Timezone" $(cat /etc/timezone 2>/dev/null)
 
 cpucount=$(grep -c processor /proc/cpuinfo)
 memory=$(grep MemTotal: /proc/meminfo | awk '{printf("%d\n", $2/1024/1024 + 0.5)}')
@@ -19,7 +21,7 @@ result_inventory "RAID Vendor" $(/usr/lib/nagios/plugins/check_raid | grep OK: |
 # so we do not need to replace whitespaces here!
 result_inventory "Server Type" $(/usr/sbin/dmidecode -s system-product-name | grep -v '^#' | head -1 | sed "s/ /_/g")
 
-result_inventory "CPU Type" $(/usr/sbin/dmidecode -s processor-version | grep -v "^#" | head -1 | sed "s/^ *//;s/ *$//;s/ /_/g")
+result_inventory "CPU Type" $(/usr/sbin/dmidecode -s processor-version | grep -v "^#" | head -1 | sed "s/ /_/g")
 
 # Slotted Uptime (7d,14d,31d,3m,6m,1y). This can help spotting issues per HW type/age/server role.
 days=$(( $(cat /proc/uptime  | cut -d . -f 1) / 86400))
@@ -36,8 +38,7 @@ if [ "$days" != "" ]; then
 
         if [ "$result" != "" ]; then
                 result_inventory "Uptime" $result
-        else   
+        else
                 result_inventory "Uptime" 365
         fi
 fi
-
