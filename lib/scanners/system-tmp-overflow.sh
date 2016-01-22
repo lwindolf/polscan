d0ef3343f44d15686fceb0ec71a5f3b075dd1364
Lars Windolf <lars.windolf@gmx.de>
2016-01-22 16:35:15 +0100

    New policy to detect /tmp overflow mounts

---
+++ lib/scanners/system-tmp-overflow.sh
@@ -0,0 +1,10 @@
+# group: System
+# name: Overflow tmp
+# description: There should be no overflow mounted /tmp. This happens when you have no /tmp partition and / runs full. In this case the kernel mounts an 1MB /tmp partition which is unusable in normal operation.
+# solution-cmd: umount overflow
+# source: http://jarrodoverson.com/blog/overflow-filesystem-in-linux/
+# source: http://stackoverflow.com/questions/17536139/tmp-mounted-with-only-1mb-space-100-used-as-filesystem-overflow
+
+if grep -q "^overflow.*/tmp" /proc/mounts; then
+	result_failed "/tmp is overflow mounted!"
+fi
