9156f00f9f1cc267eb09cc9cd9e10572c4d54eeb
Lars Windolf <lars.windolf@gmx.de>
2016-01-30 20:19:16 +0100

    Adding Apache SSL configuration policies

---
+++ lib/scanners/security-apache-forbidden-ciphers.sh
@@ -0,0 +1,19 @@
+# group: Security
+# name: Apache Forbidden Ciphers
+# description: An Apache production webserver should not allow those ciphers: aNULL, eNULL, EXPORT, DES, MD5, PSK, RC4
+# source: https://raymii.org/s/tutorials/Strong_SSL_Security_On_nginx.html
+# source: http://security.stackexchange.com/questions/51680/optimal-web-server-ssl-cipher-suite-configuration
+
+for dir in /etc/apache2 /usr/local/apache2/conf /usr/local/apache/conf; do
+	if [ -d $dir ]; then
+		if rgrep -q "^[^#]*SSLCipherSuite" $dir/*-enabled $dir/conf.d 2>/dev/null; then
+			missing=
+			for c in aNULL eNULL EXPORT DES MD5 PSK RC4; do 
+				if ! rgrep -q "ssl_ciphers[[:space:]][[:space:]].*\!$c" $dir/*-enabled $dir/conf.d 2>/dev/null; then
+					missing="${missing}!$c "
+				fi
+			done
+			result_failed "$dir: ssl_ciphers does not have $missing"
+		fi
+	fi
+done
---
+++ lib/scanners/security-apache-sslprotocol.sh
@@ -0,0 +1,30 @@
+# group: Security
+# name: Apache SSL Protocol
+# description: An Apache production webserver should disable legacy protocols
+
+failed=0
+for dir in /etc/apache2 /usr/local/apache2/conf /usr/local/apache/conf; do
+	if [ -d $dir ]; then
+		# There should be no SSLProtocol lines without the following patterns
+		for m in "-SSLv2" "-SSLv3"
+		do
+			if [ "$(rgrep SSLProtocol $dir/ | grep -v -- "$m")" != "" ]; then
+				result_failed "SSLProtocol should include $m ($dir)"
+				failed=1
+			fi
+		done
+
+		if ! rgrep -q "[^#]*SSLHonorCipherOrder.*on" $dir; then
+			result_failed "SSLHonorCipherOrder should be on ($dir)"
+		fi
+
+		# POODLE disabled
+		if ! rgrep -q "[^#]*SSLCompression.*off" $dir; then
+			result_failed "SSLCompression should be off ($dir)"
+		fi
+	fi
+done
+
+if [ "$failed" = 0 ]; then
+	result_ok
+fi
