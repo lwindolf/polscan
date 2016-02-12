6237d675f34ebaada5da9f52c99139cb7c485ba4
Lars Windolf <lars.windolf@gmx.de>
2016-02-12 18:21:51 +0100

    Add CCE for Apache server tokens policy

--- lib/scanners/security-apache-server-tokens.sh
+++ lib/scanners/security-apache-server-tokens.sh
@@ -1,7 +1,7 @@
 # group: Security
 # name: Apache ServerTokens
 # description: An Apache production webserver should not give details in the 'Server:' header
-# tags: SV-36672r1_rule
+# tags: SV-36672r1_rule CCE-27425-8
 # solution-cmd: a2enconf security
 
 for dir in /etc/apache2 /usr/local/apache2/conf /usr/local/apache/conf; do
