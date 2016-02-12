d7639668821613ca9b1d0b8ed1a049aeac4b7c4d
Lars Windolf <lars.windolf@gmx.de>
2016-02-12 18:14:53 +0100

    Policy for safe NFS exports

---
+++ lib/scanners/network-nfs-safe-exports.sh
@@ -0,0 +1,19 @@
+# group: Network
+# name: Safe NFS Exports
+# description: Ensures no NFS export has no_root_squash set
+# solution: Remove no_root_squash from /etc/exports and run 'exportfs -a'
+# tags: CCE-27138-7 CCE-27121-3 CCE-27167-6
+
+results=
+
+for i in no_root_squash insecure insecure_locks; do
+	if /bin/grep -q "$i" /etc/exports >/dev/null 2>&1; then
+		results="$results no_root_squash"
+	fi
+done
+
+if [ "$results" != "" ]; then
+	result_critical "Unsafe NFS export options: $results"
+else
+	result_ok
+fi
