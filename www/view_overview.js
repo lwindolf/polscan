14eebf1dad89ce70b147655cc7ee704f8ad42dde
Lars Windolf <lars.windolf@gmx.de>
2016-02-24 21:06:02 +0100

    Moving pie chart function to overview view as it is only use there. Checking for undefined host count.

--- www/view_overview.js
+++ www/view_overview.js
@@ -5,6 +5,56 @@ views.OverviewView = function OverviewView(parentDiv, params) {
 	this.parentDiv = parentDiv;
 };
 
+function addPieChart(id, title, size, pColor, data) {
+	return new d3pie(id, {
+		"header": {
+			"title": {
+				"text": title,
+				"color": "#fff",
+			},
+		},
+		"size": {
+			"canvasWidth": size*0.8,
+			"canvasHeight": size,
+			"pieOuterRadius": "90%"
+		},
+			"data": {
+			"sortOrder": "value-desc",
+			"content": data,
+		},
+		"tooltips": {
+			"enabled": true,
+			"type": "placeholder",
+			"string": "{label}: \n{value}, {percentage}%"
+		},
+		"labels": {
+			"outer": {
+				"format": "none"
+			},
+			"inner": {
+				"format": "label-percentage2",
+				"hideWhenLessThanPercentage": 8
+			},
+			"mainLabel": {
+				"fontSize": 16,
+			},
+			"percentage": {
+				"color": pColor,
+				"decimalPlaces": 0
+			},
+			"value": {
+				"color": "#adadad",
+				"fontSize": 10
+			}
+		},
+		'callbacks': {
+			'onClickSegment': function(segment) {
+				setLocationHash({ fG: segment.data.label});
+			}
+		}
+	});
+}
+
 views.OverviewView.prototype.update = function(params) {
 	clean();
 	$(this.parentDiv).css('margin-left:0;');
