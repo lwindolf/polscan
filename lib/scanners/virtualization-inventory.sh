055e6fb44f3ea70e3b3ea59c9de4704f9ed23993
Lars Windolf <lars.windolf@gmx.de>
2016-04-05 15:59:04 +0200

    New inventory for virtualization types

---
+++ lib/scanners/virtualization-inventory.sh
@@ -0,0 +1,35 @@
+# group: Virtualization
+# name: Inventory
+# description: Inventory only scanner virtualization instance count (containers, VMs) and type (KVM, LXC, docker...)
+
+vtype=
+while [ 1 ]; do
+	# Check KVM/libvirt
+	if virsh -version >/dev/null 2>&1; then
+		instances=$(virsh list --name | grep -c -v "^$")
+		vtype=KVM
+		break
+	fi
+
+	# Check docker
+	if docker version >/dev/null 2>&1; then
+		instances=$(docker info 2>/dev/null | grep Images | sed 's/.*\://')
+		vtype=Docker
+		break
+	fi
+
+	# Check LXC
+	out=$(lxc-ls >/dev/null 2>&1)
+	if [ "$out" != "" ]; then
+		instances=$(echo "$out" | wc -l)
+		vtype=LXC
+		break
+	fi
+
+	break
+done
+
+if [ "$vtype" != "" ]; then
+	result_inventory "Type" "$vtype"
+	result_inventory "Count" "$instances"
+fi
