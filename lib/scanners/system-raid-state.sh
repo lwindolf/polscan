bc0ea611eec73d09d4e2754198b70e10fd47b11e
Lars Windolf <lars.windolf@gmx.de>
2016-04-15 12:08:39 +0200

    Fix result call.

--- lib/scanners/system-raid-state.sh
+++ lib/scanners/system-raid-state.sh
@@ -6,7 +6,7 @@ if [ -x /usr/lib/nagios/plugins/check_raid ]; then
 	output=$(/usr/lib/nagios/plugins/check_raid)
 	case $? in
 		1) result_warning "RAID State" "$output" ;;
-		2) result_critical "RAID State" "$output" ;;
+		2) result_failed "RAID State" "$output" ;;
 		0) result_ok "RAID State" "$output";;
 	esac
 fi
