ad4fe502ef301742bd00fae42a245986bdf732a4
Lars Windolf <lars.windolf@gmx.de>
2016-05-11 16:07:14 +0200

    Add iptables rule count to network inventory.

--- lib/scanners/network-inventory.sh
+++ lib/scanners/network-inventory.sh
@@ -1,6 +1,6 @@
 # group: Network
 # name: Inventory
-# description: Inventory only scanner determining external IPv4 IPs and all routes
+# description: Inventory only scanner determining external IPv4 IPs, all routes and iptables rule count
 
 ips=$(
 	# FIXME: Extremly cheap private net matching...
@@ -11,3 +11,11 @@ ips=$(
 result_inventory "External IPs" "$ips"
 
 result_inventory "Routes" "$(/sbin/ip route | awk '{printf "%s_%s_%s ", $1, $2, $3}')"
+
+result_inventory "iptables Rule Count" "$(
+	(
+	iptables -L -n --line-numbers | grep '^[0-9]'
+	iptables -L -n --line-numbers -t nat | grep '^[0-9]'
+	# FIXME: What about other tables?
+	) | wc -l
+)"
