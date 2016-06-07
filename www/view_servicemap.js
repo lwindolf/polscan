acd2c78ee4b57a833d4af1ba1b72391ed4c5b407
Lars Windolf <lars.windolf@gmx.de>
2016-06-07 13:34:57 +0200

    Add internal unresolved and external IPs to service map.

--- www/view_servicemap.js
+++ www/view_servicemap.js
@@ -8,11 +8,12 @@ views.ServicemapView = function ServicemapView(parentDiv, params) {
                 filterby: true,
                 search: true
 	};
-	this.netMapData = {};
+	this.edges = [];
+	this.services = {};
+	this.uniqueEdges = {};
 };
 
-views.ServicemapView.prototype.updateGraph = function(services, connections) {
-	var nid = {};
+views.ServicemapView.prototype.updateGraph = function() {
 	var width = $('#netmap').width();
 	var height = $('#netmap').height();
     var svg = d3.select("#netmap").append("svg")
@@ -26,27 +27,32 @@ views.ServicemapView.prototype.updateGraph = function(services, connections) {
 				.setGraph({ "rankdir": "LR", "ranksep": 75, "nodesep": 12, "marginx": 20, "marginy": 20, "align": "DL" })
 				.setDefaultEdgeLabel(function() { return {}; });
 
-	$.each(services, function(i, n) {
-		if(n.service in nid)
-			return;
-		if(n.used === 0)
-			return;
-
+	$.each(this.services, function(i, n) {
 		var props = { "label": n.service, "labelType": "html", "class": n.class };
+		$.each(n.ips, function(i, ip) {
+			if(i < 8) {
+				if (ip.match(/^(10\.|172\.|192\.)/))
+					props.label += "<br/>"+ip;
+				else
+					props.label += '<br/><a class="resolve" href="javascript:lookupIp(\''+ip+'\')" title="Click to resolve IP">'+ip+"</a> ";
+			}
+
+			if(i == 8)
+				props.label += "<br/><span style='color:#444; font-size:small'>("+(n.ips.length - 8)+" more ...)</span>";
+		});
 		g.setNode(i, props);
-		nid[n.service] = i;
 	});
 
-	$.each(connections, function(i, l) {
+	$.each(this.edges, function(i, l) {
 		if(l.source === undefined || l.target === undefined)
 			return;
+		// FIXME: maybe enable this
 		if(l.source === l.target)
 			return;
-		console.log("connections.push("+JSON.stringify(l));
 		var props = { lineInterpolate: 'basis' };
 		if("high" !== l.port)
 			props.label = l.port;
-		g.setEdge(nid[l.source], nid[l.target], props);
+		g.setEdge(l.source, l.target, props);
 	});
 
 	var render = new dagreD3.render();
@@ -54,6 +60,29 @@ views.ServicemapView.prototype.updateGraph = function(services, connections) {
 
 	var xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
 	svg.attr("height", g.graph().height + 40);
+}
+
+views.ServicemapView.prototype.addUniqueEdge = function(fields, service, resolvedService) {
+	var escaped = service.replace(/ /g, '');
+	var escapedT = resolvedService.replace(/ /g, '');
+	var ke = fields[5] + "_" + service + "_" + fields[2] + "_" + resolvedService + "_" + fields[4];
+	if(!this.uniqueEdges.hasOwnProperty(ke)) {
+		this.uniqueEdges[ke] = 1;
+		if(fields[5] === 'out')
+			this.edges.push({ direction: fields[5], source: escaped, target: escapedT, port: fields[4] });
+		else
+			this.edges.push({ direction: fields[5], target: escaped, source: escapedT, port: fields[2] });
+	}
+}
+
+views.ServicemapView.prototype.addUniqueService = function(program, port, c, ip) {
+	// FIXME: really safe hash keys!
+	var escaped = program.replace(/ /g, '');
+	if(!this.services.hasOwnProperty(escaped))
+		this.services[escaped] = { "service": program, "port": port, "class": c, "ips": [] };
+
+	if(ip)
+		this.services[escaped]['ips'].push(ip);
 }
 
 views.ServicemapView.prototype.addHosts = function(filteredHosts) {
@@ -69,8 +98,6 @@ views.ServicemapView.prototype.addHosts = function(filteredHosts) {
 	getData("Network", function(data) {
 		var nodeportToProgram = {};
 		var portToProgram = new Array();
-		var services = {};
-		var edges = [];
 		$.each(data.results, function(i, item) {
 			if(item.policy == "Connections" && (-1 !== filteredHosts.indexOf(item.host))) {
 				var connections = item.message.split(/ /);
@@ -83,8 +110,8 @@ views.ServicemapView.prototype.addHosts = function(filteredHosts) {
 						if(fields[5] === 'in')
 							continue;
 						// For now we do not visualize local connections
-						if(fields[1] == fields[3])
-							continue;
+//						if(fields[1] == fields[3])
+//							continue;
 						if(!nodeportToProgram.hasOwnProperty(fields[1]+"_"+fields[2])) {
 							nodeportToProgram[fields[1]+"_"+fields[2]] = program;
 						}
@@ -94,18 +121,16 @@ views.ServicemapView.prototype.addHosts = function(filteredHosts) {
 		});
 
 		$.each(data.results, function(i, item) {
-			var cunique = {};
-			var count = 0;
 			if(item.policy == "Connections" && (-1 !== filteredHosts.indexOf(item.host))) {
 				var connections = item.message.split(/ /);
 				for(var c in connections) {
 					var fields = connections[c].split(/:/);
 					if(fields[5]) {
-						var id, port = fields[2], program = fields[0];
+						var port = fields[2], program = fields[0];
 
 						// For now we do not visualize local connections
-						if(fields[1] == fields[3])
-							continue;
+//						if(fields[1] == fields[3])
+//							continue;
 
 						// Resolve program for close-wait, time-wait listings
 						if(program !== "-" && !(port in portToProgram))
@@ -114,44 +139,29 @@ views.ServicemapView.prototype.addHosts = function(filteredHosts) {
 							program = portToProgram[port];
 
 						// Reduce connections to service<->service connections
-						id = program+":::"+port;
-						var s;
-						if(program !== "-")
-							s = program;
-						else
+						if(program === "-")
 							continue; 	// displaying unknown procs is just useless
-						if(!services.hasOwnProperty(id))
-							services[id] = { service: s, "port": port, "class": "local" };
-//						else if (!name.match(/^(10\.|172\.|192\.)/))
+
+						view.addUniqueService(program, port, 'local');
 
 						var resolvedService = nodeportToProgram[fields[3]+"_"+fields[4]];
 						if(resolvedService) {
-							if(fields[5] === 'out') {
-								ke = 'out'+s+'_'+resolvedService+'_'+fields[4];
-								e = { source: s, target: resolvedService, port: fields[4] };
-							} else {
-								ke = 'in'+s+'_'+resolvedService+'_'+port;
-								e = { target: s, source: resolvedService, port: port };
-							}
-							if(!cunique.hasOwnProperty(ke)) {
-								cunique[ke] = 1;
-								edges.push(e);
-								count++;
-console.log("Adding "+ke+" ("+count+"/250)");
-							} else {
-//console.log("Skipping "+e+" ("+count+"/250)");
-}
+								view.addUniqueEdge(fields, program, resolvedService);
 						} else {
-//							console.log("unresolved: "+fields[3]+":"+fields[4]);
+							if(fields[3].match(/^(10\.|172\.|192\.)/)) {
+								view.addUniqueService('Unresolved Internal '+fields[5], undefined, 'other', fields[3]);
+								view.addUniqueEdge(fields, program, 'Unresolved Internal '+fields[5]);
+							} else {
+								view.addUniqueService('External '+fields[5], undefined, 'other', fields[3]);
+								view.addUniqueEdge(fields, program, 'External '+fields[5]);
+							}
 						}
 					}
 				}
-				if(count > 250)
-					return;
 			}
 		});
 
-		view.updateGraph(services, edges);
+		view.updateGraph();
 	});
 }
 
