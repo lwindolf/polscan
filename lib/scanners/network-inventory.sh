f90ec83141bccd9ef8e77fa2ac92265a87e3e439
Lars Windolf <lars.windolf@gmx.de>
2016-06-16 18:53:05 +0200

    Adding nameservers and IPv6 address count to network inventory.

--- lib/scanners/network-inventory.sh
+++ lib/scanners/network-inventory.sh
@@ -1,6 +1,6 @@
 # group: Network
 # name: Inventory
-# description: Inventory only scanner determining external IPv4 IPs and all routes
+# description: Inventory only scanner determining external IPv4 IPs, all routes, all nameservers and the number of configured scope global IPv6 addresses
 
 ips=$(
 	# FIXME: Extremly cheap private net matching...
@@ -11,3 +11,7 @@ ips=$(
 result_inventory "External IPs" "$ips"
 
 result_inventory "Routes" "$(/sbin/ip route | awk '{printf "%s_%s_%s ", $1, $2, $3}')"
+
+result_inventory "Name Servers" $(awk '/^nameserver/ {print $2}' /etc/resolv.conf)
+
+result_inventory "IPv6 Address" "$(ip a |grep 'inet6 .*scope global' | wc -l)"
