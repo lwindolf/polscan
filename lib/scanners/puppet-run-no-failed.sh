202965a99f60eaf0fb5a3208d54f9fa584596305
Lars Windolf <lars.windolf@gmx.de>
2016-10-28 15:10:36 +0200

    Detect entirely missing runs too

--- lib/scanners/puppet-run-no-failed.sh
+++ lib/scanners/puppet-run-no-failed.sh
@@ -3,17 +3,21 @@
 # description: Checks for Puppet 2/3 last_run_report.yaml for failed resources
 
 puppet_report=/var/lib/puppet/state/last_run_report.yaml
-if [ -f $puppet_report ]; then 
-	failed=$(nl $puppet_report 2>/dev/null | grep 'status: failure')
-	resources=
-	while read f; do
-		# Ugly YAML parsing by backtracking resource...
-		resources="$resources,$(nl $puppet_report | grep -B100 "$f" | grep 'resource:' | sed 's/.*resource://;s/"//g' | tail -1)"
-	done < <(/bin/echo "$failed" | grep -v "^$")
-
-	if [ "$resources" != "" ]; then
-		result_failed "Failed resources:" ${resources/,/}
+if [ -f $puppet_report ]; then
+	if [ "$(find /var/lib/puppet/state -name "last_run_report.yaml" -mtime +1)" != "" ]; then
+		result_failed "No run in the last 24 hours!"
 	else
-		result_ok
+	    failed=$(nl $puppet_report 2>/dev/null | grep 'status: failure')
+	    resources=
+	    while read f; do
+		    # Ugly YAML parsing by backtracking resource...
+		    resources="$resources,$(nl $puppet_report | grep -B100 "$f" | grep 'resource:' | sed 's/.*resource://;s/"//g' | tail -1)"
+	    done < <(/bin/echo "$failed" | grep -v "^$")
+
+	    if [ "$resources" != "" ]; then
+		    result_failed "Failed resources:" ${resources/,/}
+	    else
+		    result_ok
+		fi
 	fi
 fi
