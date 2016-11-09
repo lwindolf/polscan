273b65f4fe2f6c6b62480f0816d964c55f36b652
Lars Windolf <lars.windolf@gmx.de>
2016-11-10 00:24:06 +0100

    Update security-nginx-certs.sh

--- lib/scanners/security-nginx-certs.sh
+++ lib/scanners/security-nginx-certs.sh
@@ -18,8 +18,8 @@ for dir in /etc/nginx /usr/local/nginx/conf; do
 			x=$(openssl x509 -in "$c" -text)
 
 			# Check for weak signature algorithm
-			if echo "$x" | grep -q "Signature Algorithm: sha[12]WithRSAEncryption"; then
-				result_failed "$c is SHA-1/2 signed which is insecure!"
+			if echo "$x" | grep -q "Signature Algorithm: sha1WithRSAEncryption"; then
+				result_failed "$c is SHA-1 signed which is insecure!"
 			elif echo "$x" | grep -q "Signature Algorithm: sha256WithRSAEncryption"; then
 				result_warning "$c is SHA-256 signed and might be insecure."
 			fi
