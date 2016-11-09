3eb6cbf750dc2e5826e7508f4dfa02cd1d5f92f7
Lars Windolf <lars.windolf@gmx.de>
2016-11-10 00:23:27 +0100

    Update security-apache-certs.sh

--- lib/scanners/security-apache-certs.sh
+++ lib/scanners/security-apache-certs.sh
@@ -17,8 +17,8 @@ for dir in /etc/apache2 /usr/local/apache2/conf /usr/local/apache/conf; do
 			x=$(openssl x509 -in "$c" -text)
 
 			# Check for weak signature algorithm
-			if echo "$x" | grep -q "Signature Algorithm: sha[12]WithRSAEncryption"; then
-				result_failed "$c is SHA-1/2 signed which is insecure!"
+			if echo "$x" | grep -q "Signature Algorithm: sha1WithRSAEncryption"; then
+				result_failed "$c is SHA-1 signed which is insecure!"
 			elif echo "$x" | grep -q "Signature Algorithm: sha256WithRSAEncryption"; then
 				result_warning "$c is SHA-256 signed and might be insecure."
 			fi
