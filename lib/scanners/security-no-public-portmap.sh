2ac9a6a2e19b3f6461e0aedbd6fe0127c2ab3dc9
Lars Windolf <lars.windolf@gmx.de>
2017-02-03 18:30:02 +0100

    Detect both names portmap and rpcbind and check port 836 too

--- lib/scanners/security-no-public-portmap.sh
+++ lib/scanners/security-no-public-portmap.sh
@@ -2,7 +2,7 @@
 # name: No public portmap
 # description: The portmap port must no be visible on external IPs.
 
-if netstat -tlpn | grep -q "0.0.0.0:111.*LISTEN.*portmap"; then
+if netstat -tlpn | egrep -q "0.0.0.0:(111|836).*LISTEN.*(portmap|rpcbind)"; then
 	ext_ips=$(ip a | grep "inet " | egrep -v "inet (172|10|192|127)")
 	if [ "$ext_ips" != "" ]; then
 		result_failed "Portmap port bound on 0.0.0.0"
