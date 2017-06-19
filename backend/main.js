6ac8c0d59f42f01e37f426e4f9839adaf1980f3a
Lars Windolf <lars.windolf@gmx.de>
2017-06-20 00:27:33 +0200

    Preparing a remote probe API.

--- backend/main.js
+++ backend/main.js
@@ -4,7 +4,9 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs"),
-    app = express();
+    app = express(),
+    promise = require("promise"),
+    StatefulProcessCommandProxy = require("stateful-process-command-proxy");
 
 var config = require('../etc/backend-config.json');
 var probes = require('../etc/probes.json');
@@ -58,6 +60,60 @@ icinga2_rest = {
     console.log("No Icinga2 monitoring API configured.");
 }
 
+// Remote server probe API
+function probe(request, response) {
+
+   var probe = request.params.probe;
+   var host = request.params.host;
+
+   if(!(probe in probes)) {
+      response.writeHead(404, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify({error:'No such probe'}));
+      return;
+   }
+   
+   var cmd = probes[probe].command;
+   var proxy = new StatefulProcessCommandProxy(
+    {
+      name: "proxy_"+host,
+      max: 1,
+      min: 1,
+      idleTimeoutMS: 15000,
+
+      logFunction: function(severity,origin,msg) {
+          console.log(severity.toUpperCase() + " " +origin+" "+ msg);
+      },
+
+      processCommand: '/usr/bin/ssh',
+      processArgs:  [host],
+      processRetainMaxCmdHistory : 0,
+
+      processInvalidateOnRegex :
+          {
+            'stderr':[{regex:'.*error.*',flags:'ig'}]
+          },
+
+      processCwd : './',
+      processUid : null,
+      processGid : null,
+      initCommands : ['LANG=C;echo'],	// to catch banners and pseudo-terminal warnings
+
+      validateFunction: function(processProxy) {
+          return processProxy.isValid();
+      },
+   });
+
+   proxy.executeCommands(['hostname',cmd]).then(function(res) {
+      response.writeHead(200, {'Content-Type': 'application/json'});
+      // FIXME really process res, not just sending it
+      response.end(JSON.stringify(res));
+   }).catch(function(error) {
+      console.log("proxy failed:"+error);
+      response.writeHead(422, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify({"error":error}));
+   });
+}
+
 // Query Icinga2 API
 function monitoring_api(request, response) {
    response.writeHead(200, {'Content-Type': 'application/json'});
@@ -89,6 +145,10 @@ function static_content(type, path, response) {
       response.end("File not found");
    }
 }
+
+app.get('/api/probe/:probe/:host', function(req, res) {
+   probe(req, res);
+});
 
 app.get('/api/monitoring', function(req, res) {
    monitoring_api(req, res);
