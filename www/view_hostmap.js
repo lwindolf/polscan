df321fd247a9572a8b2cec8522ae3df06f78c4e4
Lars Windolf <lars.windolf@gmx.de>
2017-09-15 16:24:50 +0200

    Add legend feature for hostmaps too.
    - Click to filter is still missing.

--- www/view_hostmap.js
+++ www/view_hostmap.js
@@ -46,6 +46,10 @@ views.HostmapView.prototype.tooltip = function(container, event) {
 
 views.HostmapView.prototype.addHostsToMap = function(params) {
 	var view = this;
+	this.topFindings = {};
+	this.legendColorIndex = {};
+        this.legendSelection = [];
+
 	getData(params.fG, function(data) {
 		var findingsByHost = new Array();
 		var filteredHosts = get_hosts_filtered(params, true)
@@ -55,7 +59,19 @@ views.HostmapView.prototype.addHostsToMap = function(params) {
 		// 3 times failed and 1 warning
 		$.each(data.results, function(i, item) {
 			findingsByHost[item.host] += item.severity.substring(0,1);
+			var topKey = item.severity+":::"+(item.group?item.group:data.group)+':::'+item.policy;
+			if('OK' !== item.severity) {
+				if(undefined === view.topFindings[topKey]) {
+					var i = Object.keys(view.topFindings).length;
+					view.legendColorIndex[i] = i;
+					view.topFindings[topKey] = 0;
+				}
+				view.topFindings[topKey]++;
+			}
 		});
+
+		if(undefined === filteredHosts)
+			filteredHosts = Object.keys(findingsByHost);
 
 		for(h in filteredHosts) {
 			var host = filteredHosts[h];
@@ -92,6 +108,20 @@ views.HostmapView.prototype.addHostsToMap = function(params) {
 			$(this).find('.wcount').html($(this).find('.boxes .hostMapBox.WARNING').length);
 		});
 
+                // Create colors for numeric legend by title
+                // and for non-numeric legends by index
+		var numeric = 0;
+                var lastElem = view.topFindings[view.topFindings.length-1];
+		var i = 0;
+		Object.keys(view.topFindings).sort().forEach(function(name) {
+                        var count = view.topFindings[name];
+			var tmp = name.split(/:::/);
+			var colorClass = tmp[0];
+                        $('#legend').append("<span class='legendItem legendIndex"+i+"' title='"+tmp[1]+" - "+tmp[2]+"'>"+tmp[1]+' - '+tmp[2]+" ("+count+")</span>");
+                        $('.legendIndex'+i).addClass(colorClass);
+			i++;
+                });
+
 		installTooltip('.hostMapBox', view.tooltip);
 	    $("#hostmap").tablesorter({sortList: [[1,1],[2,1],[3,1],[0,0]]});
 	});
@@ -104,7 +134,7 @@ views.HostmapView.prototype.update = function(params) {
 		if(!params.gT)
 			params.gT = "Domain";	// This usually does exist
 
-		$(this.parentDiv).append('<table id="hostmap" class="resultTable tablesorter"><thead><tr><th>Group</th><th>C</th><th>W</th><th>Nr</th></tr></thead></table><div id="selectedGroup"/>');
+		$(this.parentDiv).append('<div id="legend" title="Click to filter a legend item. Hold Ctrl and click to multi-select."><b>Legend</b></div><table id="hostmap" class="resultTable tablesorter"><thead><tr><th>Group</th><th>C</th><th>W</th><th>Nr</th></tr></thead></table><div id="selectedGroup"/>');
 		this.addHostsToMap(params);
 	} else {
 		$(this.parentDiv).append('<h2>Findings By Group</h2>');
