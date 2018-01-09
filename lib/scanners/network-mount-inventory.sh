0cecb6d00b9f98a8167000af728b389067b755c2
Lars Windolf <lars.windolf@gmx.de>
2018-01-09 18:31:50 +0100

    Add NFS exports to Mount Point edge inventory. Drop duplicate scanner.

--- lib/scanners/network-mount-inventory.sh
+++ lib/scanners/network-mount-inventory.sh
@@ -1,10 +1,12 @@
 # group: Network
 # name: Inventory
-# description: Inventory only scanner determining distributed filesystem mount points from /proc/mounts
+# description: Inventory only scanner determining distributed filesystem mount points from /proc/mounts and mount clients on NFS exports using showmount
 
 awk '$1 ~ /:/ {print}' /proc/mounts |\
 while read remote local type rest; do
 	result_network_edge "Mount Point" "$local" "$(hostname -f)" "mount" "$remote" "$type" "out" 1
 done
 
-
+while read ip share; do
+	result_network_edge "Mount Point" "nfs" "$(hostname -f)" "high" "$ip" "NFS Share $share" "in" 1
+done < <(test -f /sbin/showmount && /sbin/showmount --no-headers | sed "s/\:/ /")
