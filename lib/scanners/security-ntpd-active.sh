5ade3944522c703cb94aa2d7d5360fe7e96ec528
Lars Windolf <lars.windolf@gmx.de>
2018-10-04 16:39:49 +0200

    Support chronyd as NTP server too.

--- lib/scanners/security-ntpd-active.sh
+++ lib/scanners/security-ntpd-active.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-
 # group: Security
 # name: ntpd on
-# description: There must be a running ntpd
+# description: There must be a running NTP server (as ntpd or chronyd)
 # tags: CCE-27093-4
 # solution-cmd: apt-get install ntp; /etc/init.d/ntp start
 
-if ! pgrep -f ntpd >/dev/null; then
-	result_failed "No running 'ntpd' process found!"
+if ! pgrep -f "(ntpd|chronyd)" >/dev/null; then
+	result_failed "No running NTP process found!"
+else
+	result_ok
 fi
