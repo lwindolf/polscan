02d86656412a3f2f1d5efcfb9c01d91bb644125d
Lars Windolf <lars.windolf@gmx.de>
2018-10-16 13:48:21 +0200

    Do not fail Puppet check if no state dir found

--- lib/scanners/puppet-run-not-disabled.sh
+++ lib/scanners/puppet-run-not-disabled.sh
@@ -5,12 +5,12 @@
 # description: Checks for Puppet 2/3/4 lock file indicating Puppet runs are disabled. Also checks if the puppet daemon is running.
 # solution-cmd: /usr/bin/puppet agent --enable
 
-if ! pgrep puppet >/dev/null; then
-	result_failed "No Puppet process running!"
-fi
-
 puppet_state_dir=$(dirname "$puppet_report" 2>/dev/null)
 if [ -d "$puppet_state_dir" ]; then
+	if ! pgrep puppet >/dev/null; then
+		result_failed "No Puppet process running!"
+	fi
+
 	if [ ! -f "$puppet_state_dir/puppetdlock" -a ! -f "$puppet_state_dir/agent_disabled.lock" ]; then
 		result_ok
 	else
