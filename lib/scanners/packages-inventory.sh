ee0bd1d6cc57159d954499c4b0c2db17eab76a5d
Lars Windolf <lars.windolf@gmx.de>
2018-10-18 11:41:33 +0200

    Make Debian repo scanners neutral on non-Debian

--- lib/scanners/packages-inventory.sh
+++ lib/scanners/packages-inventory.sh
@@ -2,9 +2,11 @@
 
 # group: Packages
 # name: Inventory
-# description: Inventory only scanner for 3rd party repos
+# description: Inventory only scanner for Debian 3rd party repos
 
 PACKAGES_INVENTORY_3RD_PARTY_REPOS_WHITELIST=${PACKAGES_INVENTORY_3RD_PARTY_REPOS_WHITELIST-nowhitelistconfigured}
 
-result_inventory "3rd Party Repos" $(egrep -hv "^#|debian.org|aptrepo" /etc/apt/sources.list /etc/apt/sources.list.d/* | sed 's/.*htt[ps]*:\/\///;s/[ \/].*//;s/^deb\.//;s/^packages\.//' | sort -u)
+if [ -d /etc/apt/ ]; then
+	result_inventory "3rd Party Repos" $(egrep -hv "^#|debian.org|aptrepo" /etc/apt/sources.list /etc/apt/sources.list.d/* | sed 's/.*htt[ps]*:\/\///;s/[ \/].*//;s/^deb\.//;s/^packages\.//' | sort -u)
+fi
 
--- lib/scanners/security-repo-enabled.sh
+++ lib/scanners/security-repo-enabled.sh
@@ -4,8 +4,10 @@
 # name: Security Repo
 # description: Ensure the security.debian.org repo is enabled
 
-if egrep -q '^deb[[:space:]]*(http://security.debian.org|http://archive.debian.org.*security).*main' /etc/apt/sources.list /etc/apt/sources.list.d/* 2>/dev/null; then
-	result_ok
-else
-	result_failed "Debian security updates repo not found in /etc/apt/source.list*!"
+if [ -d /etc/apt ]; then
+	if egrep -q '^deb[[:space:]]*(http://security.debian.org|http://archive.debian.org.*security).*main' /etc/apt/sources.list /etc/apt/sources.list.d/* 2>/dev/null; then
+		result_ok
+	else
+		result_failed "Debian security updates repo not found in /etc/apt/source.list*!"
+	fi
 fi
