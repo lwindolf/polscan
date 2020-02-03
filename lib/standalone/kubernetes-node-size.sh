f5636ded7af4b36367ad7150df2b56934de036e0
Lars Windolf <lars.windolf@gmx.de>
2020-02-03 22:20:22 +0100

    Fix jq command overlay

--- lib/standalone/kubernetes-node-size.sh
+++ lib/standalone/kubernetes-node-size.sh
@@ -8,20 +8,20 @@ source $(dirname $0)/../scanner-functions.inc
 
 k8s_node_size() {
 	JSON=$(timeout -k 10 -s 9 5 kubectl get nodes -o json)
-	NODES=$(jq "$JSON" '.items[].metadata.name')
+	NODES=$(json "$JSON" '.items[].metadata.name')
 	for n in $NODES; do
 		/bin/echo "Processing $n" >&2
-		cpucount=$(jq "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.capacity.cpu')
-		memory=$(jq "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.capacity.memory')
+		cpucount=$(json "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.capacity.cpu')
+		memory=$(json "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.capacity.memory')
 		(
 			echo "|||CPU RAM|||" "${cpucount}x${memory}"
-			echo "|||OS Release|||" "$(jq "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.nodeInfo.osImage' | sed "s/ /_/g")"
-			echo "|||kubelet Version|||" "$(jq "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.nodeInfo.kubeletVersion')"
-			echo "|||Container Runtime|||" "$(jq "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.nodeInfo.containerRuntimeVersion')"
-			echo "|||Kernel Version|||" "$(jq "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.nodeInfo.kernelVersion')"
-			echo "|||Region|||" "$(jq "$JSON" '.items[] | select(.metadata.name == "'$n'") | .metadata.labels["failure-domain.beta.kubernetes.io/region"]')"
-			echo "|||Zone|||" "$(jq "$JSON" '.items[] | select(.metadata.name == "'$n'") | .metadata.labels["failure-domain.beta.kubernetes.io/zone"]')"
-			echo "|||Instance Type|||" "$(jq "$JSON" '.items[] | select(.metadata.name == "'$n'") | .metadata.labels["beta.kubernetes.io/instance-type"]')"
+			echo "|||OS Release|||" "$(json "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.nodeInfo.osImage' | sed "s/ /_/g")"
+			echo "|||kubelet Version|||" "$(json "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.nodeInfo.kubeletVersion')"
+			echo "|||Container Runtime|||" "$(json "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.nodeInfo.containerRuntimeVersion')"
+			echo "|||Kernel Version|||" "$(json "$JSON" '.items[] | select(.metadata.name == "'$n'") | .status.nodeInfo.kernelVersion')"
+			echo "|||Region|||" "$(json "$JSON" '.items[] | select(.metadata.name == "'$n'") | .metadata.labels["failure-domain.beta.kubernetes.io/region"]')"
+			echo "|||Zone|||" "$(json "$JSON" '.items[] | select(.metadata.name == "'$n'") | .metadata.labels["failure-domain.beta.kubernetes.io/zone"]')"
+			echo "|||Instance Type|||" "$(json "$JSON" '.items[] | select(.metadata.name == "'$n'") | .metadata.labels["beta.kubernetes.io/instance-type"]')"
 		) | sed "s/^/$n System INVENTORY /"
 	done
 }
